0a6ff177a615e94128983dc7d96d068284f10b91
dake <chen423@yeah.net>
2020-08-11 23:40:00 +0800

    code fix

--- TCKit/TCUIKit/UI/UIView+TCHelper.h
+++ TCKit/TCUIKit/UI/UIView+TCHelper.h
@@ -34,6 +34,82 @@ extern NSString *const kTCFooterIdentifier;
 
 @end
 
+
+
+typedef NS_ENUM(NSInteger, TCMenuElementState) {
+    TCMenuElementStateOff,
+    TCMenuElementStateOn,
+    TCMenuElementStateMixed
+};
+
+typedef NS_OPTIONS(NSUInteger, TCMenuElementAttributes) {
+    TCMenuElementAttributesDefault      = 0,
+    TCMenuElementAttributesDisabled     = 1 << 0,
+    TCMenuElementAttributesDestructive  = 1 << 1,
+    TCMenuElementAttributesHidden       = 1 << 2,
+    
+    
+    
+    TCMenuElementAttributesCancel       = 1 << 20,
+};
+
+typedef NS_OPTIONS(NSUInteger, TCMenuOptions) {
+    /// Show children inline in parent, instead of hierarchically
+    TCMenuOptionsDisplayInline  = 1 << 0,
+
+    /// Indicates whether the menu should be rendered with a destructive appearance in its parent
+    TCMenuOptionsDestructive    = 1 << 1,
+};
+
+ 
+@class TCUIAction;
+typedef void (^TCUIActionHandler)(__kindof TCUIAction *action);
+
+@interface TCUIAction : NSObject
+
+/// Short display title.
+@property (nullable, nonatomic, copy) NSString *title;
+
+@property (nullable, nonatomic, copy) NSString *titleWithoutIcon;
+
+/// Image that can appear next to this action.
+@property (nullable, nonatomic, copy) UIImage *_Nullable (^imageBlock)(void);
+
+/// Elaborated title, if any.
+@property (nullable, nonatomic, copy) NSString *discoverabilityTitle;
+
+/// This action's identifier.
+@property (nullable, nonatomic, copy) NSString *identifier;
+
+/// This action's style.
+@property (nonatomic, assign) TCMenuElementAttributes attributes;
+
+@property (nullable, nonatomic, strong) NSNumber/*TCMenuElementAttributes*/ *attributesWithoutIcon;
+
+@property (nonatomic, assign) TCMenuElementState state;
+
+@property (nonatomic, assign) TCMenuOptions options;
+
+@property (nullable, nonatomic, copy) TCUIActionHandler handler;
+
+@property (nullable, nonatomic, strong) NSArray<TCUIAction *> *children;
+
+- (BOOL)hasNextLevelMenu;
+
+- (__kindof UIMenuElement *)UIMenuElement API_AVAILABLE(ios(13.0));
+- (NSArray<UIAlertAction *> *)UIAlertActions;
+
+//- (UIAction *)UIAction:(UIActionHandler)handler API_AVAILABLE(ios(13.0));
+//- (UIContextualAction *)UIContextualAction:(UIContextualActionHandler)handler API_AVAILABLE(ios(11.0));
+//- (UITableViewRowAction *)UITableViewRowAction:(void (^)(UITableViewRowAction *action, NSIndexPath *indexPath))handler;
+//- (UIAlertAction *)UIAlertAction:(void (^ __nullable)(UIAlertAction *action))handler;
+//- (UIPreviewAction *)UIPreviewAction:(void (^)(UIPreviewAction *action, UIViewController *previewViewController))handler;
+
+// UIBarItem block 封装
+
+@end
+
+
 NS_ASSUME_NONNULL_END
 
 
--- TCKit/TCUIKit/UI/UIView+TCHelper.m
+++ TCKit/TCUIKit/UI/UIView+TCHelper.m
@@ -123,4 +123,131 @@ static char const kAlignmentRectInsetsKey;
 @end
 
 
+
+@implementation TCUIAction
+
+- (BOOL)hasNextLevelMenu
+{
+    return self.children.count > 0 && 0 == (TCMenuOptionsDisplayInline & self.options);
+}
+
+- (UIMenuOptions)UIMenuOptions API_AVAILABLE(ios(13.0))
+{
+    TCMenuOptions options = self.options;
+    if (options == 0) {
+        return 0;
+    }
+    
+    UIMenuOptions opt = 0;
+    if (0 != (TCMenuOptionsDisplayInline & options)) {
+        opt |= UIMenuOptionsDisplayInline;
+    }
+    if (0 != (TCMenuOptionsDestructive & options)) {
+        opt |= UIMenuOptionsDestructive;
+    }
+    return opt;
+}
+
+- (UIMenuElementAttributes)UIMenuElementAttributes API_AVAILABLE(ios(13.0))
+{
+    TCMenuElementAttributes attrib = self.attributes;
+    if (attrib == 0) {
+        return 0;
+    }
+    
+    UIMenuElementAttributes attr = 0;
+    if (0 != (TCMenuElementAttributesDisabled & attrib)) {
+        attr |= UIMenuElementAttributesDisabled;
+    }
+    if (0 != (TCMenuElementAttributesDestructive & attrib)) {
+        attr |= UIMenuElementAttributesDestructive;
+    }
+    if (0 != (TCMenuElementAttributesHidden & attrib)) {
+        attr |= UIMenuElementAttributesHidden;
+    }
+    return attr;
+}
+
+- (UIMenuElementState)UIMenuElementState API_AVAILABLE(ios(13.0))
+{
+    switch (self.state) {
+        case TCMenuElementStateOff:
+            return UIMenuElementStateOff;
+        case TCMenuElementStateOn:
+            return UIMenuElementStateOn;
+        case TCMenuElementStateMixed:
+            return UIMenuElementStateMixed;
+            
+        default:
+            return UIMenuElementStateOff;
+    }
+}
+
+// TODO: cancel 等老方式才有的 item 怎么转换
+// TODO: UIKeyCommand UIDeferredMenuElement
+- (__kindof UIMenuElement *)UIMenuElement API_AVAILABLE(ios(13.0))
+{
+    if (self.children.count < 1) {
+        UIImage *icon = nil != self.imageBlock ? self.imageBlock() : nil;
+        UIAction *action = [UIAction actionWithTitle:self.title ?: @"" image:icon identifier:self.identifier handler:^(__kindof UIAction * _Nonnull action) {
+            // force retain
+            if (nil != self.handler) {
+                self.handler(self);
+            }
+        }];
+        action.discoverabilityTitle = self.discoverabilityTitle;
+        action.attributes = self.UIMenuElementAttributes;
+        action.state = self.UIMenuElementState;
+        return action;
+    }
+    
+    NSCParameterAssert(nil == self.handler);
+    NSMutableArray<UIMenuElement *> *items = NSMutableArray.array;
+    for (TCUIAction *action in self.children) {
+        [items addObject:action.UIMenuElement];
+    }
+    
+    UIImage *icon = nil != self.imageBlock ? self.imageBlock() : nil;
+    UIMenu *menu = [UIMenu menuWithTitle:self.title ?: @"" image:icon identifier:self.identifier options:self.UIMenuOptions children:items];
+    return menu;
+}
+
+- (NSArray<UIAlertAction *> *)UIAlertActions
+{
+    if (self.hasNextLevelMenu) {
+        TCMenuElementAttributes attr = nil != self.attributesWithoutIcon ? self.attributesWithoutIcon.unsignedIntegerValue : self.attributes;
+        UIAlertActionStyle style = 0 != (TCMenuOptionsDestructive & self.options) ? UIAlertActionStyleDestructive : (0 != (TCMenuElementAttributesCancel & attr) ? UIAlertActionStyleCancel : UIAlertActionStyleDefault);
+        UIAlertAction *action = [UIAlertAction actionWithTitle:self.titleWithoutIcon ?: self.title style:style handler:^(UIAlertAction * _Nonnull action) {
+            // force retain
+            if (nil != self.handler) {
+                self.handler(self);
+            }
+        }];
+        action.enabled = 0 == (TCMenuElementAttributesDisabled & attr);
+        return @[action];
+    }
+    
+    NSMutableArray<UIAlertAction *> *items = NSMutableArray.array;
+    if (self.title.length > 0 && (nil != self.handler || self.children.count < 1)) {
+        TCMenuElementAttributes attr = nil != self.attributesWithoutIcon ? self.attributesWithoutIcon.unsignedIntegerValue : self.attributes;
+        UIAlertActionStyle style = 0 != (TCMenuElementAttributesDestructive & attr) ? UIAlertActionStyleDestructive : (0 != (TCMenuElementAttributesCancel & attr) ? UIAlertActionStyleCancel : UIAlertActionStyleDefault);
+        UIAlertAction *action = [UIAlertAction actionWithTitle:self.titleWithoutIcon ?: self.title style:style handler:^(UIAlertAction * _Nonnull action) {
+            // force retain
+            if (nil != self.handler) {
+                self.handler(self);
+            }
+        }];
+        action.enabled = 0 == (TCMenuElementAttributesDisabled & attr);
+        [items addObject:action];
+    }
+    for (TCUIAction *action in self.children) {
+        [items addObjectsFromArray:action.UIAlertActions];
+    }
+    
+    return items;
+}
+
+@end
+
+
 #endif
